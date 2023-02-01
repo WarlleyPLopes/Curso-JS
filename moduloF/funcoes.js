6e2c4281601c813f16b7f338eb5497b3cbfa1a2f
Warlley Lopes <warlleylopes123@gmail.com>
2023-02-01 19:12:16 -0300

    update

--- moduloF/funcoes.js
+++ moduloF/funcoes.js
@@ -1,11 +1,11 @@
-function parimpa(n) {
+function parImpa(n) {
   if (n % 2 == 0) {
     return `Par!`;
   } else {
     return `Impar!`;
   }
 }
-console.log(parimpa(223));
+console.log(parImpa(223));
 /* let res = parimpa(4);
 console.log(res);
  */
